bd0aa95a3da3d566fe2f006381496f3a069813c6
Ashik150 <sskhan4409@gmail.com>
2026-08-16 02:15:03 +0600

    new

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,3 +1,5 @@
+import logger from "../utils/logger.js";
+
 class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -10,12 +12,12 @@ export const errorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
   if (err.name === "CastError") {
-    const message = `Resource not found. Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
+    const message = `Resource not found. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
   }
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
   }
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again!`;
@@ -25,6 +27,15 @@ export const errorMiddleware = (err, req, res, next) => {
     const message = `Json Web Token is expired, Try again!`;
     err = new ErrorHandler(message, 400);
   }
+
+  logger.error({
+    message: err.message,
+    statusCode: err.statusCode,
+    path: req.originalUrl,
+    method: req.method,
+    stack: err.stack,
+  });
+
   return res.status(err.statusCode).json({
     success: false,
     message: err.message,
